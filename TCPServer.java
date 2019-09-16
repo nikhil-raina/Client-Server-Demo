5c0bd39b94c7685edaf6aed0bb541c1413e82fae
nikhilraina <nxr5013@g.rit.edu>
2019-09-15 20:48:11 -0400

    Success in receiving arraylist from client.

--- TCPServer.java
+++ TCPServer.java
@@ -1,6 +1,7 @@
 import java.net.*;
 import java.io.*;
 import java.nio.Buffer;
+import java.util.ArrayList;
 
 public class TCPServer {
     public static void main(String[] args) {
@@ -10,16 +11,19 @@ public class TCPServer {
             ServerSocket listenSocket = new ServerSocket(serverPort);
             Socket clientSocket = listenSocket.accept();
             int i = 0;
-            while(i < 100){
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+            ArrayList result = (ArrayList) ois.readObject();
+            System.out.println(result);
+//             while(i < 100){
+//                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                System.out.println(in.readLine());
-                out.println(i);
-                i++;
-//                Connection c = new Connection(clientSocket);
-            }
-        } catch (IOException e) {
+//                 System.out.println(in.readLine());
+//                 out.println(i);
+//                 i++;
+// //                Connection c = new Connection(clientSocket);
+//             }
+        } catch (Exception e) {
             System.out.println("Listen: " + e.getMessage());
         } 
     }
